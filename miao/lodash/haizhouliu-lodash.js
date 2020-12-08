6681a0a2b659c7d068aea6730fa766fec93d94a8
administrator <administrator@qq.com>
2020-12-08 15:51:15 +0800

    first add lodash

---
+++ miao/lodash/haizhouliu-lodash.js
@@ -0,0 +1,14 @@
+var haizhouliu = (function () {
+  function compact(ary) {
+    let result = [];
+    for (let i = 0; i < ary.length; i++) {
+      if (ary[i]) {
+        result.push(ary[i]);
+      }
+    }
+    return result;
+  }
+  return {
+    compact,
+  };
+})();
